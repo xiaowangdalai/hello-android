8c37145e5fd495ee9a49320a69e730b843c1222c
Zach <ljbcg@qq.com>
2015-07-14 01:25:57 +0800

    a

---
+++ hello-android/src/com/xwdl/hello/SlidingMenuActivity2.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) FantasyValley. All Rights Reserved.
+ *
+ * SlidingMenuActivity2.java
+ */
+package com.xwdl.hello;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Walden
+ * @since 1.6
+ */
+public class SlidingMenuActivity2 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_sliding_menu_2);
+	}
+}
