d244ecf28941059caaa924848a9b30959bb3f517
Zach <liujia@richinfo.cn>
2015-07-20 17:52:41 +0800

    add moji3 entrance.

---
+++ hello-android/src/com/xwdl/hello/Moji3Activity.java
@@ -0,0 +1,14 @@
+package com.xwdl.hello;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Moji3Activity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_moji3);
+	}
+}
--- hello-android/src/com/xwdl/hello/PropertyAnimationListActivity.java
+++ hello-android/src/com/xwdl/hello/PropertyAnimationListActivity.java
@@ -6,52 +6,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class PropertyAnimationListActivity extends Activity {
 	
 	private static final String[] LIST_DATA = new String[] {
-		"侧滑菜单",
-		"属性动画"
+		"墨迹天气3.0"
 	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 		
-		initView();
-	}
-
-	private void initView() {
-		ListView list = (ListView) findViewById(R.id.hello_list);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), 
+		setContentView(R.layout.activity_property_animation_list);
+		
+		ListView list = (ListView) findViewById(R.id.list);
+		ListAdapter adapter = new ArrayAdapter<String>(getApplicationContext(), 
 				R.layout.my_simple_list_item_1, LIST_DATA);
 		list.setAdapter(adapter);
 		
 		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+			public void onItemClick(AdapterView<?> arg0, View v, int position, long arg3) {
 				switch (position) {
 				case 0:
-					openSlidingMenuListPage();
-					break;
-					
-				case 1:
-					startActivity(new Intent(MainActivity.this, PropertyAnimationListActivity.class));
+					startActivity(new Intent(PropertyAnimationListActivity.this,
+							Moji3Activity.class));
 					break;
 
 				default:
 					break;
 				}
 			}
+			
 		});
 	}
-	
-	private void openSlidingMenuListPage() {
-		Intent it = new Intent(this, SlidingMenuListActivity.class);
-		startActivity(it);
-	}
-
 }
