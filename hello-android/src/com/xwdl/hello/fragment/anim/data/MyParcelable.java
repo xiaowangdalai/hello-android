e760049d5ce59af91a947a90e7f3a62a10f3aee3
Zach <liujia@richinfo.cn>
2015-08-07 15:45:17 +0800

    aaa

--- hello-android/src/com/xwdl/hello/fragment/anim/data/MyParcelable.java
+++ hello-android/src/com/xwdl/hello/fragment/anim/data/MyParcelable.java
@@ -46,9 +46,10 @@ import android.os.Parcelable;
  * 
  * 6、Parcelable接口定义
  * 
- * 复制代码 public interface Parcelable { //内容描述接口，基本不用管 public int
- * describeContents(); //写入接口函数，打包 public void writeToParcel(Parcel dest, int
- * flags); //读取接口，目的是要从Parcel中构造一个实现了Parcelable的类的实例处理。因为实现类在这里还是不可知的，
+ * public interface Parcelable { 
+ * //内容描述接口，基本不用管 
+ * public int describeContents(); //写入接口函数，打包 
+ * public void writeToParcel(Parcel dest, int flags); //读取接口，目的是要从Parcel中构造一个实现了Parcelable的类的实例处理。因为实现类在这里还是不可知的，
  * 所以需要用到模板的方式，继承类名通过模板参数传入 //为了能够实现模板参数的传入，这里定义Creator嵌入接口,
  * 内含两个接口函数分别返回单个和多个继承类实例 public interface Creator<T> { public T
  * createFromParcel(Parcel source); public T[] newArray(int size); } } 复制代码
@@ -154,7 +155,7 @@ public class MyParcelable implements Parcelable {
 	}
 
 	@Override
-	public void writeToParcel(Parcel arg0, int arg1) {
+	public void writeToParcel(Parcel dest, int flags) {
 	}
 
 }
