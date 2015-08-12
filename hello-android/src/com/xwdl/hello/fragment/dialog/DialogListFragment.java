875cf662c11b982b3850462e18353602b17ce2df
Zach <liujia@richinfo.cn>
2015-08-12 17:18:48 +0800

    popupwindow

--- hello-android/src/com/xwdl/hello/fragment/dialog/DialogListFragment.java
+++ hello-android/src/com/xwdl/hello/fragment/dialog/DialogListFragment.java
@@ -1,22 +1,29 @@
 package com.xwdl.hello.fragment.dialog;
 
-import com.xwdl.hello.R;
-
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.Toast;
+
+import com.xwdl.hello.R;
 
 public class DialogListFragment extends Fragment {
 	
 	private static final String[] DATA = new String[] {
-		"Dialog Fragment"
+		"Dialog Fragment",
+		"Pop Window"
 	};
 	
 	private Context mContext;
@@ -43,11 +50,34 @@ public class DialogListFragment extends Fragment {
 		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+			public void onItemClick(AdapterView<?> arg0, View view, int position, long arg3) {
 				switch (position) {
 				case 0:
 					DialogFragment1 df = DialogFragment1.newInstance();
 					df.show(getFragmentManager(), "dialog");
+					break;
+					
+				case 1:
+					LinearLayout contentView = new LinearLayout(mContext);
+					contentView.setBackgroundColor(0xff000000);
+					
+					Button aText = new Button(getActivity());
+					aText.setText("弹出菜单");
+					aText.setOnClickListener(new OnClickListener() {
+						
+						@Override
+						public void onClick(View v) {
+							Toast.makeText(mContext, "aaa", Toast.LENGTH_SHORT).show();
+						}
+					});
+					contentView.addView(aText);
+					
+					PopupWindow window = new PopupWindow(contentView, LayoutParams.WRAP_CONTENT, 
+							LayoutParams.WRAP_CONTENT, true);
+//					window.setTouchable(true);
+					window.setOutsideTouchable(true);
+					window.setBackgroundDrawable(getResources().getDrawable(R.drawable.popupwindow_bg));
+					window.showAsDropDown(view);
 					break;
 
 				default:
